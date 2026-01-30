0aeb879fea3302f2199335122f58e6175dea84a2
bishalghale98 <bishalghle@gmail.com>
2026-01-30 12:39:06 +0545

    feat: Initialize Express application with core middleware, routing, health check, Swagger documentation, and error handling.

--- src/app.ts
+++ src/app.ts
@@ -22,20 +22,9 @@ app.set('trust proxy', 1);
 // Security Middlewares
 app.use(helmet()); // Set security HTTP headers
 
-const allowedOrigins = [
-    env.CORS_ORIGIN,
-];
-
 app.use(
     cors({
-        origin: function (origin, callback) {
-            // allow requests with no origin (like Postman)
-            if (!origin || allowedOrigins.includes(origin)) {
-                callback(null, true);
-            } else {
-                callback(new Error("Not allowed by CORS"));
-            }
-        },
+        origin: "*",
         credentials: true, // Allow cookies to be sent
     })
 );
