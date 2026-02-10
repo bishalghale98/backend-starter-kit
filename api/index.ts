fa8fcc9bc27c9ec9fc43e382e9ffe5cb0ac48739
bishalghale98 <bishalghle@gmail.com>
2026-02-11 00:57:56 +0545

    gg

--- api/index.ts
+++ api/index.ts
@@ -1,9 +1,3 @@
 import app from '../src/app';
-import { prisma } from '../src/config/dbConnect';
-
-app.get('/api/vhealth', async (req, res) => {
-    const users = await prisma.user.findMany({ take: 1 })
-    res.json({ ok: true, users })
-})
 
 export default app;
